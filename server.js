fcb2b0eccf60ac96e2e97af9026448b7b03a6ec3
Elwin-Alcazar <janelwin_alcazar@yahoo.com>
2021-03-26 11:56:19 +1300

    create temp route for results

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const routes = require('./routes')
+const express = require("express");
+const hbs = require("express-handlebars");
+const routes = require("./routes");
+const fs = require("fs");
 
-const server = express()
-module.exports = server
+const server = express();
+module.exports = server;
 
 // Middleware
-server.engine('hbs', hbs({
-  extname: 'hbs'
-}))
-server.set('view engine', 'hbs')
-server.use(express.static('public'))
+server.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+  })
+);
+server.set("view engine", "hbs");
+server.use(express.static("public"));
 
 // Routes
-server.get('/', (req, res) => {
-  const viewData = {
-    title: 'Gallery'
-  }
-  const template = 'home'
-  res.render(template, viewData)
-})
+server.get("/", (req, res) => {
+  const viewData = {};
+  const template = "home";
+  res.render(template, viewData);
+});
+
+server.get("/results", (req, res) => {
+  fs.readFile("./restaurants.json", "utf-8", (err, data) => {
+    const parsedData = JSON.parse(data);
+    res.render("results", parsedData);
+  });
+});
